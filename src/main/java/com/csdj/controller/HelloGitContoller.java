e207a3d5fdca35f5887f6590ef01231deaf4acd3
Zengdinggen <Zengdinggen@163.com>
2024-04-24 15:43:24 +0800

    新创建的 Controller类 进行第三次提交 hot-fix 分支

--- src/main/java/com/csdj/controller/HelloGitContoller.java
+++ src/main/java/com/csdj/controller/HelloGitContoller.java
@@ -9,5 +9,9 @@ public class HelloGitContoller {
         System.out.println("你好！Git!!");
         System.out.println("你好！Git!!");
         System.out.println("你好！Git!!");
+        int  a = 10;
+        int b = 20;
+        int c = a +b;
+        System.out.println(c);
     }
 }
